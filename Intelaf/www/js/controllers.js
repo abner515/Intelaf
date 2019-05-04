464b68d3fb1fc1bc0303f1ef5bc615e89b92b2a0
abner515 <santosabner515@gmail.com>
2019-05-04 09:19:04 -0600

    que

---
+++ Intelaf/www/js/controllers.js
@@ -0,0 +1,52 @@
+angular.module('starter.controllers', [])
+
+.controller('CategoriesCtrl', function($scope) {
+
+  //categories
+  $scope.categoria = [
+    "Telefonia",
+    "Impresoras",
+    "Gaming",
+    "Computadoras",
+    "Accesorios",
+    "Componentes PC",
+    "Muebles y Oficina",
+    "Camaras"
+  ];
+  $scope.cat = [
+  {
+    nombre: 'Telefonia',
+    icon: 'fas fa-mobile'
+  },{
+    nombre: 'Impresora',
+    icon: 'fa fa-print'
+  
+  }]
+
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
